358d683620bffd7269976be414ed3e647ca08dc8
Stefanodt94 <stefanodt944@outlook.com>
2025-07-22 09:26:38 +0200

    Updates

---
+++ _app/immutable/nodes/5.-GFUJswR.js
@@ -0,0 +1,27 @@
+import{_ as f}from"../chunks/DKe1jUWt.js";import{f as c,a as m}from"../chunks/C-pdriqz.js";import"../chunks/hHgKIxiL.js";import{o as _}from"../chunks/CgQsw0Sc.js";import{a8 as k,a9 as L,af as S,ag as y,ah as u,N as t,M as z,ai as $,O as s,aj as P}from"../chunks/Dd1TZjzD.js";import{h as I}from"../chunks/CeG0xfVu.js";import{s as n}from"../chunks/ZrHj00C5.js";import{i as A}from"../chunks/C-Cec4J2.js";import{S as B}from"../chunks/CEgpYe12.js";import{b as d}from"../chunks/ClGkAc0s.js";var D=c('<meta name="description" content="Lerne Sandra kennen – Fotografin mit Leidenschaft und einem Auge fürs Detail."/>'),E=c(`<div id="page" class="relative bg-dark-overlay flex flex-col md:flex-row items-center gap-8 p-8 overflow-hidden svelte-9d4qgu"><img alt="Sfondo Fotografia - Sandra Fotografin" class="absolute inset-0 object-cover w-full h-full z-[-1]" loading="eager"/> <div class="flex-1 z-10 flex justify-center items-center"><img class="w-full max-w-md rounded-lg shadow-lg" alt="Sandra"/></div> <div class="flex-1 z-10 text-white lg:mr-30"><h1 class="text-6xl md:text-8xl lg:text-10xl font-extralight mb-4 titel svelte-9d4qgu">Hallo, ich bin Sandra!</h1> <p class="satz mb-4 font-extralight tracking-widest text-xl md:text-2xl lg:text-4x">Ich liebe es, besondere Momente mit meiner Kamera festzuhalten. Ob
+          Hochzeit, Familienfotos oder kreative Portraits. Ich erzähle
+          Geschichten mit Licht und Emotion.</p> <div class="mt-10"><a class="text-white tracking-widest text-lg md:text-xl lg:text-2xl bg-transparent rounded border border-[#B8860B] active:scale-[0.95] hover:bg-[#B8860B] transition duration-200 p-4 cursor-pointer shadow-xl cursor-pointer">Termin Buchen</a></div></div></div> <div class="flex flex-col flex-1 bg-white w-full"><div class="grid grid-cols-1 md:grid-cols-2 px-4 pt-4 md:px-10 pt-10 lg:px-20 pt-20"><div class="flex flex-col mx.auto w-full bg-black p-4 md:p-10 lg:p-20"><p class="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor a
+            odit doloribus ipsam praesentium delectus enim alias, eaque et quis
+            accusamus at voluptatem rem quia, sequi provident placeat, vero
+            quos! Ipsa eius sit a alias ducimus, deleniti vitae culpa eligendi
+            obcaecati, unde eaque nihil dolores est reiciendis cum. Laborum
+            mollitia, dolor dolorum animi aut ducimus sed maxime tenetur
+            aspernatur modi illum vel omnis culpa obcaecati.</p></div> <div class="flex flex-col mx.auto w-full bg-gray-200 p-4 md:p-10 lg:p-20"><p class="text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
+            molestiae nesciunt harum hic! Quia nostrum ipsam molestias vel sunt.
+            Quaerat, saepe alias culpa aliquid aperiam sequi maxime iusto
+            laborum? Nemo doloribus a quod. Corrupti deleniti ex ipsum facere
+            dolores eum expedita praesentium beatae reprehenderit officia vitae
+            at numquam, iure tempora, eos animi consequuntur dignissimos! Magnam
+            et quia eum explicabo nulla, rem architecto sunt sequi.</p></div></div> <div class="grid grid-cols-1 md:grid-cols-2 px-4 pb-4 md:px-10 pb-10 lg:px-20 pb-20"><div class="flex flex-col mx.auto w-full bg-gray-200 p-4 md:p-10 lg:p-20"><p class="text-black">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor a
+            odit doloribus ipsam praesentium delectus enim alias, eaque et quis
+            accusamus at voluptatem rem quia, sequi provident placeat, vero
+            quos! Ipsa eius sit a alias ducimus, deleniti vitae culpa eligendi
+            obcaecati, unde eaque nihil dolores est reiciendis cum. Laborum
+            mollitia, dolor dolorum animi aut ducimus sed maxime tenetur
+            aspernatur modi illum vel omnis culpa obcaecati.</p></div> <div class="flex flex-col mx.auto w-full bg-black p-4 md:p-10 lg:p-20"><p class="text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
+            molestiae nesciunt harum hic! Quia nostrum ipsam molestias vel sunt.
+            Quaerat, saepe alias culpa aliquid aperiam sequi maxime iusto
+            laborum? Nemo doloribus a quod. Corrupti deleniti ex ipsum facere
+            dolores eum expedita praesentium beatae reprehenderit officia vitae
+            at numquam, iure tempora, eos animi consequuntur dignissimos! Magnam
+            et quia eum explicabo nulla, rem architecto sunt sequi.</p></div></div></div>`,1),F=c("<main><!></main>");function G(v,b){k(b,!1),_(async()=>{const{gsap:e}=await f(async()=>{const{gsap:a}=await import("../chunks/acJusjrU.js");return{gsap:a}},[],import.meta.url),{SplitText:i}=await f(async()=>{const{SplitText:a}=await import("../chunks/CctIO7Dn.js");return{SplitText:a}},[],import.meta.url);await $(),e.registerPlugin(i),e.from("#page",{autoAlpha:0,duration:.5,ease:"expo.out"});const o=new i(".satz",{type:"lines",lineClass:"line",autoSplit:!0,mask:"lines"});e.from(o.lines,{yPercent:200,autoAlpha:0,stagger:{each:.08,from:"start"}})}),A();var r=F();I(e=>{var i=D();S.title="Über mich – Sandra Fotostudio",m(e,i)});var h=t(r);B(h,{children:(e,i)=>{var o=E(),a=y(o),p=t(a),l=u(p,2),q=t(l);s(l);var x=u(l,2),g=u(t(x),4),w=t(g);s(g),s(x),s(a),P(2),z(()=>{n(p,"src",`${d??""}/assets/lens.webp`),n(q,"src",`${d??""}/assets/sandra.webp`),n(w,"href",`${d??""}/kontakt`)}),m(e,o)},$$slots:{default:!0}}),s(r),m(v,r),L()}export{G as component};
